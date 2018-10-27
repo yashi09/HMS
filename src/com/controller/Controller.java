95d558e849c7f4af67fb725e8d89894eac8aaa26
Yashi Sharma <yashi09999@gmail.com>
2018-10-28 04:11:39 +0530

    Fix Controller

--- src/com/controller/Controller.java
+++ src/com/controller/Controller.java
@@ -1,14 +1,29 @@
 package com.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class Controller extends HttpServlet{
 	
-	/*private void doget(HttpServletRequest request, HttpServletResponse response) throws IOException {
-	//	doPost(request, response);
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		
 	}
-	
-	private void dopost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		//doGet(request,response);
-	}*/
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
 }
